a2100d9eed706740a78d922897a86bd93f6c522c
Ariel D. Salazar H <adayler@gmail.com>
2013-06-11 23:42:49 -0400

    Initial commit

---
+++ UnittestDatabaseSample/RollbackAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Transactions;
+using NUnit.Framework;
+
+namespace UnittestDatabaseSample
+{
+    /// <summary>
+    /// Rollback Attribute wraps test execution into a transaction and cancels the transaction once the test is finished.
+    /// You can use this attribute on single test methods or test classes/suites
+    /// </summary>
+    public class RollbackAttribute : Attribute, ITestAction
+    {
+        private TransactionScope transaction;
+
+        public void BeforeTest(TestDetails testDetails)
+        {
+            transaction = new TransactionScope();
+        }
+
+        public void AfterTest(TestDetails testDetails)
+        {
+            transaction.Dispose();
+        }
+
+        public ActionTargets Targets
+        {
+            get { return ActionTargets.Test; }
+        }
+    }
+}
---
+++ UnittestDatabaseSample/TestDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using NUnit.Framework;
+
+namespace UnittestDatabaseSample
+{
+    [TestFixture]
+    class TestDatabase
+    {
+        private const String TestSp = "p_InsertTestData";
+        private const String ConnectionString = "Server=.\\SQL2008;Database=TestUnitTestDB;User Id=sa;Password=control;";
+
+        private SqlConnection connection;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // No op.
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // No op.
+        }
+
+        [Test, Rollback]
+        public void TestInsertTestData()
+        {
+            connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            Assert.DoesNotThrow(delegate
+                {
+                    using (SqlCommand command = new SqlCommand(TestSp, connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.Add(new SqlParameter("@LogText", "Lelya te extraño..."));
+                        command.ExecuteNonQuery();
+                    }
+                });
+
+            connection.Close();
+        }
+    }
+}
